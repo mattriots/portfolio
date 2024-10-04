be0988e10c62b1fa55cfed43d38762a2861b0308
mattriots <mattdepauw@gmail.com>
2024-10-04 14:45:13 -0700

    added new skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,6 +8,10 @@ import ReactImg from '../assets/react.png';
 import Tailwind from '../assets/tailwind.png';
 import Github from '../assets/github.png';
 import Mongo from '../assets/mongo.png';
+import Docker from '../assets/docker.png';
+import Java from '../assets/java.png';
+import Python from  '../assets/python.png';
+import SQL from '../assets/sql.png';
 
 const Skills = () => {
    return (
@@ -16,7 +20,6 @@ const Skills = () => {
          <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
             <div>
                <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">Skills</p>
-               {/* <p className="py-4"> // These are the technoligies that I've worked with.</p> */}
             </div>
 
             <div className="w-full grid grid-cols-3 sm:grid-cols-4 gap-4 text-center py-8">
@@ -51,6 +54,22 @@ const Skills = () => {
                <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
                   <img className="w-20 mx-auto" src={Mongo} alt="Mongo" />
                   <p className="my-4">MONGO DB</p>
+               </div>
+               <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                  <img className="w-20 mx-auto mt-4" src={Docker} alt="Docker" />
+                  <p className="my-5">DOCKER</p>
+               </div>
+               <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                  <img className="w-20 mx-auto mt-1 " src={Java} alt="Java" />
+                  <p className="my-3">JAVA</p>
+               </div>
+               <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                  <img className="w-20 mx-auto mt-1 " src={Python} alt="Python" />
+                  <p className="my-3">PYTHON</p>
+               </div>
+               <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                  <img className="h-16 mx-auto mt-3 " src={SQL} alt="SQL" />
+                  <p className="mt-5">SQL</p>
                </div>
             </div>
          </div>
