4d92cbe2af70e7c130b50a1fcfbb969a61a570e2
mattriots <mattdepauw@gmail.com>
2024-10-04 12:40:31 -0700

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+   return (
+      <footer className="bg-[#0a192f] text-white py-4 text-center mt-auto">
+         <p className="text-sm">© {new Date().getFullYear()} Matt DePauw. All rights reserved.</p>
+      </footer>
+   );
+};
+
+export default Footer;
